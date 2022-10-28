0955ad318a98fada8fc376bdf208026be31a698e
Panth Patel <ppate783@my.centennialcollege.ca>
2022-10-28 09:47:59 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,10 @@
+//301197341
+//Midterm
+//Panth Patel
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb+srv://Panth:3Y42vyPJ73iBtLYE@cluster0.5c14emi.mongodb.net/books229?retryWrites=true&w=majority"
+  "URI": "mongodb+srv://Panth:3Y42vyPJ73iBtLYE@cluster0.5c14emi.mongodb.net/books229?retryWrites=true&w=majority"
+};
+//3Y42vyPJ73iBtLYE
+//LmTvaxbfNCdf23KH
---
+++ server/models/books.js
@@ -0,0 +1,18 @@
+//301197341
+//Midterm
+//Panth Patel
+let mongoose = require('mongoose');
+
+// create a model class
+let Book = mongoose.Schema({
+    Title: String,
+    Description: String,
+    Price: Number,
+    Author: String,
+    Genre: String
+},
+{
+  collection: "books"
+});
+
+module.exports = mongoose.model('Book', Book);
